3315c8ae93e29fef466d82bfab51039929a9dfe2
Dan Johnson <djboy71@gmail.com>
2014-01-25 11:27:50 -0600

    Initial commit

---
+++ src/main/java/com/springinpractice/ch04/web/AccountForm.java
@@ -0,0 +1,143 @@
+package com.springinpractice.ch04.web;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class AccountForm {
+
+	private String username;
+	private String password;
+	private String confirmPassword;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	private boolean marketingOk = true;
+	private boolean acceptTerms = false;
+	
+	public String getUsername() {
+		
+		return username;
+		
+	} // end getUsername()
+	
+	
+	public void setUsername(String username) {
+		
+		this.username = username;
+		
+	} // end setUsername()
+	
+	
+	public String getPassword() {
+		
+		return password;
+		
+	} // end getPassword()
+	
+	
+	public void setPassword(String password) {
+		
+		this.password = password;
+		
+	} // end setPassword()
+	
+	
+	public String getConfirmPassword() {
+		
+		return confirmPassword;
+		
+	} //end getConfirmPassword()
+	
+	
+	public void setConfirmPassword(String confirmPassword) {
+		
+		this.confirmPassword = confirmPassword;
+		
+	} // end setConfirmPassword()
+	
+	
+	public String getFirstName() {
+		
+		return firstName;
+		
+	} // end getFirstName()
+	
+	
+	public void setFirstName(String firstName) {
+		
+		this.firstName = firstName;
+		
+	} // end setFirstName()
+	
+	
+	public String getLastName() {
+		
+		return lastName;
+		
+	} //end getLastName()
+	
+	
+	public void setLastName(String lastName) {
+		
+		this.lastName = lastName;
+		
+	} // end setLastName()
+	
+	
+	public String getEmail() {
+		
+		return email;
+		
+	} // end setEmail()
+	
+	
+	public void setEmail(String email) {
+		
+		this.email = email;
+		
+	} // end setEmail()
+	
+	
+	public boolean getMarketingOk() {
+		
+		return marketingOk;
+		
+	} // end getMarketingOk()
+	
+	
+	public void setMarketingOk(boolean marketingOk) {
+		
+		this.marketingOk = marketingOk;
+		
+	} // end setMarketingOk()
+	
+	
+	public boolean getAcceptTerms() {
+		
+		return acceptTerms;
+		
+	} // end getAcceptTerms()
+	
+	
+	public void setAcceptTerms(boolean acceptTerms) {
+		
+		this.acceptTerms = acceptTerms;
+		
+	} // end setAcceptTerms()
+	
+	
+	public String toString() {
+		
+		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+			.append("username", username)
+			.append("firstName", firstName)
+			.append("lastName", lastName)
+			.append("email", email)
+			.append("marketingOk", marketingOk)
+			.append("acceptTeerms", acceptTerms)
+			.toString();
+		
+	} // end toString()
+	
+} // end AccountForm class
